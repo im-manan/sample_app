84e6a293cfd4577397c7f81d19540619b74cd1de
MananCoder29 <mananshah2961998@gmail.com>
2023-01-08 12:44:01 +0530

    end of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,22 @@
+import React, {useState} from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
 import './styles.css';
 
 function App() {
+  // const title = React.createElement('h1', {}, 'Revents no JSX')
+  // const div = React.createElement('div', {className: 'App'}, title);
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
-    <div className="App">
-        <h1>Re-vents</h1>
-    </div>
+    <>
+        <NavBar setFormOpen = {setFormOpen}/>
+        <Container className='main'>
+          <EventDashboard formOpen = {formOpen}  setFormOpen = {setFormOpen}/>
+        </Container>
+    </>
+    // div
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css'
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 function render() {
   root.render(
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   );
 }
 

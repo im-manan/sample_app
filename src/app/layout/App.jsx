f4927fd24998837658d788bb979231e798aba188
MananCoder29 <mananshah2961998@gmail.com>
2023-01-09 12:57:55 +0530

    End of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,20 +1,36 @@
-import React, {useState} from 'react';
-import { Container } from 'semantic-ui-react';
-import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar';
-import './styles.css';
+import React, { useState } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import "./styles.css";
 
 function App() {
   // const title = React.createElement('h1', {}, 'Revents no JSX')
   // const div = React.createElement('div', {className: 'App'}, title);
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
 
   return (
     <>
-        <NavBar setFormOpen = {setFormOpen}/>
-        <Container className='main'>
-          <EventDashboard formOpen = {formOpen}  setFormOpen = {setFormOpen}/>
-        </Container>
+      <NavBar setFormOpen={handleCreateFormOpen} />
+      <Container className="main">
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectEvent={handleSelectEvent}
+          selectedEvent={selectedEvent}
+        />
+      </Container>
     </>
     // div
   );
